2d5265547b32c535b0b8b3895abd8373403bfea3
Alesi <alexdelusarreta@gmail.com>
2022-10-01 18:50:53 -0300

    Hice el template y los viws

--- MVT1/views.py
+++ MVT1/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.template import Context, Template
 
 def agregar_personas(request):
-    return HttpResponse("<h1>ACA AGREGAS PERSONAS</h1>")
+    cargar_archivo = open(r"C:\Users\alexis.delusarreta\Desktop\MVT_TP1\templates\template1.html", "r")
+    template = Template(cargar_archivo.read())
+    cargar_archivo.close()
+    contexto = Context()
+    template_renderizado = template.render(contexto)
+    return HttpResponse(template_renderizado)
 
 def ver_personas(request):
     return HttpResponse("<h1>ACA VES PERSONAS</h1>")
