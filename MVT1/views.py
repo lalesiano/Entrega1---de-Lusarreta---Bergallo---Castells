291ed06223e4d9a28e263c0015d0004d555982b8
Alesi <alexdelusarreta@gmail.com>
2022-10-01 19:35:01 -0300

    Vincule el nombre con el template; a continuar clase 2 minuto 00:19:10

--- MVT1/views.py
+++ MVT1/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.template import Context, Template
 
-def agregar_personas(request):
+def agregar_personas(request, nombre):
+    cargar_archivo = open(r"C:\Users\alexis.delusarreta\Desktop\MVT_TP1\templates\template1.html", "r")
+    template = Template(cargar_archivo.read())
+    cargar_archivo.close()
+    contexto = Context({"nombre" : nombre})
+    template_renderizado = template.render(contexto)
+    return HttpResponse(template_renderizado)
+
+def ver_personas(request):
     cargar_archivo = open(r"C:\Users\alexis.delusarreta\Desktop\MVT_TP1\templates\template1.html", "r")
     template = Template(cargar_archivo.read())
     cargar_archivo.close()
     contexto = Context()
     template_renderizado = template.render(contexto)
     return HttpResponse(template_renderizado)
-
-def ver_personas(request):
-    return HttpResponse("<h1>ACA VES PERSONAS</h1>")
